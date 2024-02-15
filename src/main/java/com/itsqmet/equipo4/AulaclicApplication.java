9986eb389d0a598ca7f3ce3e3a202923d18f2dd1
SebasG18 <sebaslol.g_@hotmail.com>
2024-02-14 20:51:41 -0500

    first commit

---
+++ src/main/java/com/itsqmet/equipo4/AulaclicApplication.java
@@ -0,0 +1,13 @@
+package com.itsqmet.equipo4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaclicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaclicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itsqmet/equipo4/AulaclicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itsqmet.equipo4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaclicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
